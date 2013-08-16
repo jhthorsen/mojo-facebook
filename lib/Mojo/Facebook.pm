2572ae55a890a4b4fd35c24662dfde6ff1084eb6
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-08-16 23:22:00 +0200

    Released version 0.0401
           * Fix NAME in Makefile.PL

--- lib/Mojo/Facebook.pm
+++ lib/Mojo/Facebook.pm
@@ -6,7 +6,7 @@ Mojo::Facebook - Talk with Facebook
 
 =head1 VERSION
 
-0.04
+0.0401
 
 =head1 DESCRIPTION
 
@@ -67,7 +67,7 @@ use Mojo::UserAgent;
 use Mojo::Util qw/ url_unescape /;
 use constant TEST => $INC{'Test/Mojo.pm'};
 
-our $VERSION = '0.04';
+our $VERSION = '0.0401';
 
 =head1 ATTRIBUTES
 
